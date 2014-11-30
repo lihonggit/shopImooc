fa5bd242a2676f579aa7f524bbaf8172e3c27497
lihonggit <517303073@qq.com>
2014-11-30 19:07:13 +0800

    删除字体文件

--- shopImooc/lib/image.func.php
+++ shopImooc/lib/image.func.php
@@ -14,18 +14,12 @@ function verifyImage($type=1,$length=4,$pixel=0,$line=10,$sses_name="verify")
     imagefilledrectangle($image, 1, 1, $width - 2, $height - 2, $white);
     $chars = buildRandomString($type, $length);
     $_SESSION[$sses_name] = $chars;
-    $fontfiles = array(
-        "SIMKAI.TTF",
-        "SIMSUN.TTC",
-        "STFANGSO.TTF",
-        "STKAITI.TTF"
-    );
     for ($i = 0; $i < $length; $i ++) {
         $size = mt_rand(14, 18);
         $angle = mt_rand(- 15, 15);
         $x = 3 + $i * $size;
         $y = mt_rand(18, 22);
-        $fontfile = "../fonts/" . $fontfiles[mt_rand(0, count($fontfiles) - 1)];
+        $fontfile = "../fonts/" . "STFANGSO.TTF";
         $color = imagecolorallocate($image, mt_rand(50, 90), mt_rand(80, 200), mt_rand(90, 180));
         $text = substr($chars, $i, 1);
         imagettftext($image, $size, $angle, $x, $y, $color, $fontfile, $text);
