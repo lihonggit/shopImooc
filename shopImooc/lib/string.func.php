b4fc5f7573ad2a645293223b0bd8d8485ff60858
pojo <517303073@qq.com>
2014-12-16 11:01:19 +0800

    冲突测试

--- shopImooc/lib/string.func.php
+++ shopImooc/lib/string.func.php
@@ -14,3 +14,15 @@ function buildRandomString($type = 1, $length = 4)
     $chars = str_shuffle($chars);
     return substr($chars, 0, $length);
 }
+
+/**
+ * 生成唯一字符串heh
+ * @return string
+ */
+function getUniName(){
+    return md5(uniqid(microtime(true),true));
+}
+
+function getExt($fileName){
+    return strtolower(end(explode(".", $fileName)));
+}
